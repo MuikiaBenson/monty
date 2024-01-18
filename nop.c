bccb510937b9549220d98fb5589478833b9b758a
MuikiaBensonclear <benmuigai20@gmail.com>
2024-01-18 14:16:52 +0300

    monty

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+  *f_nop- NOthing
+  *@head: Stack head
+  *@counter: The line number
+  *Return: No return value
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
